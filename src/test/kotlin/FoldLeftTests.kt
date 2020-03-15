279dfe35b05f45efe244b624be8f5220a05a5731
Andrew Harmel-Law <andrew@harmel-law.com>
2020-03-15 19:47:20 +0000

    Tidying up and with better comments and test names.

--- src/test/kotlin/FoldLeftTests.kt
+++ src/test/kotlin/FoldLeftTests.kt
@@ -4,35 +4,35 @@ import org.junit.jupiter.api.Test
 
 class FoldLeftTests {
 
+    // We use this as a shorthand to avoid needing a function literal after the first test.  We use it with "::sum"
     private fun sum(l: Int, r: Int) : Int = l + r
 
-    // 1. Simplest version - empty list
+    // 1. The simplest way to call our foldLeft - on an empty list with an "add" function
     @Test
-    fun `fold on an empty list yields the seed`() {
-        val seed = 1;
+    fun `folding on an empty list yields the seed no matter what the function`() {
+        val seed = 1
         val emptyList: List<Int> = emptyList()
         assertEquals(seed, foldLeft(emptyList, seed, fun(a, b) = a + b))
     }
 
-    // 2. Now we can fold on a single-item list
+    // 2. Next we can fold on a single-item list
     @Test
-    fun `fold on a single-item list yields the result of folding that item into the seed`() {
-        val seed = 0;
+    fun `folding on a single-item list yields the result of folding that item into the seed`() {
+        val seed = 0
         assertEquals(seed + 1, foldLeft(listOf(1), seed, ::sum))
     }
 
     // 3. Now we can fold on a two-item list
     @Test
-    fun `fold on a two-item list yields the expected`() {
-        val seed = 0;
+    fun `folding on a two-item list yields the seed plus the first item plus the second list`() {
+        val seed = 0
         assertEquals(seed + 1 + -2 + 3, foldLeft(listOf(1, -2, 3), seed, ::sum))
     }
 
-    // 4. Now we can fold on a very large list - it doesn't stack overflow because we're tail recursing
-    // TODO - this stack overflows.  Make it stop! :(
+    // 4. Now we can fold on a very large list - it needn't stack overflow if we're tail-recursing
     @Test
-    fun `fold on a very large list`() {
-        val seed = 1;
+    fun `folding on a very large list doesn't stack overflow`() {
+        val seed = 1
         val massiveList = mutableListOf<Int>()
         for (i in 1..10000) {
             massiveList.add(i)
